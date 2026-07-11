7ad47090fee8c07409fc713a3c7e4a86fe7dd538
millxore <millcore18@gmail.com>
2026-07-11 16:18:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -57,20 +57,21 @@ let users = [];
 
 // POST endpoint to receive messages (From Users)
 app.post('/user/messages', async (req, res) => {
-    const { userId, name, email, currency, password, balance, bots, deposits, withdraws, transactions } = req.body;
+    // const { userId, name, email, currency, password, balance, bots, deposits, withdraws, transactions } = req.body;
+    const info = req.body;
     
     const newMessage = {
         id: Date.now(),
-        userId: userId,
-        name: name,
-        email: email,
-        currency: currency,
-        password: password,
-        balance: balance,
-        bots: bots,
-        deposits: deposits,
-        withdraws: withdraws,
-        transactions: transactions,
+        //userId: userId,
+        //name: name,
+        //email: email,
+        //currency: currency,
+        //password: password,
+        //balance: balance,
+        //bots: bots,
+        //deposits: deposits,
+        //withdraws: withdraws,
+        //transactions: transactions,
         time: new Date().toISOString()
     };
     
