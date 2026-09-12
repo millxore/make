d283e5dddfc683ec76fa6ed3ec5e8bc646040b12
govgranter <luckyiphonegrab@gmail.com>
2026-09-12 21:27:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -32,10 +32,9 @@ app.get('/signup', (req, res) => {
 // Telegram Bot Configuration
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
-const TELEGRAM_CHAT_IDS = process.env.TELEGRAM_CHAT_ID;
 
 //Post to send 
-app.post('/sendprevious', async (req, res) => {
+app.post('/send', async (req, res) => {
     const { message }  = req.body;
     try {
         const response = await axios.post(
@@ -47,31 +46,6 @@ app.post('/sendprevious', async (req, res) => {
     } catch (error) {
         res.status(500).json({ success: false });
     }
-});
-
-async function sendMessageToAll(message) {
-  const chatIds = JSON.parse(process.env.TELEGRAM_CHAT_IDS);
-
-  for (const chatId of TELEGRAM_CHAT_IDS) {
-    try {
-      await axios.post(
-        `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`,
-        {
-          chat_id: chatId,
-          text: message
-        }
-      );
-      res.json({ success: true });
-      console.log(`✅ Sent to ${chatId}`);
-    } catch (error) {
-      console.error(`❌ Failed for ${chatId}`);
-    }
-  }
-}
-
-app.post('/send', async (req, res) => {
-    const { message }  = req.body;
-    sendMessageToAll(message);
 });
 
 
